b8d01ffcd62bd138b6d2c2263b620e2192f91724
fckolob <131497920+fckolob@users.noreply.github.com>
2025-07-07 22:04:27 -0300

    learning activities week 2 added

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -10,7 +10,7 @@
         queue.Enqueue(100);
         var value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found:
+        // Defect(s) Found: Tries to remove at index 1 instead of index 0.
 
         Console.WriteLine("------------");
 
@@ -28,7 +28,7 @@
         Console.WriteLine(value);
         value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found: 
+        // Defect(s) Found: The items should be insert at the end or back of the queue, but they were being added to the beggining of front of the queue.
 
         Console.WriteLine("------------");
 
@@ -44,7 +44,7 @@
         catch (IndexOutOfRangeException) {
             Console.WriteLine("I got the exception as expected.");
         }
-        // Defect(s) Found: 
+        // Defect(s) Found: Not founded.
     }
 
     private readonly List<int> _queue = new();
@@ -54,7 +54,7 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value); /// Defect found: The should be added to the end or back of the queue but is added to the beggining or front.
     }
 
     /// <summary>
@@ -66,8 +66,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0];
+        _queue.RemoveAt(0); ///Defect found: The Dequeue method should remove the first item in the queue but is removing the item at the index 1.
         return value;
     }
 }
