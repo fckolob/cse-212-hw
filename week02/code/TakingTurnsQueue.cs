32c2d0faf6398a13f16a36446716e128648484b6
fckolob <131497920+fckolob@users.noreply.github.com>
2025-07-09 22:45:59 -0300

    prove activity week2 initated

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -37,17 +37,24 @@ public class TakingTurnsQueue
         {
             throw new InvalidOperationException("No one in the queue.");
         }
-        else
+        
+        Person person = _people.Dequeue();
+
+        if (person.Turns <= 0)
         {
-            Person person = _people.Dequeue();
-            if (person.Turns > 1)
+            _people.Enqueue(person);
+        }
+        
+
+            
+        else if (person.Turns > 1)
             {
-                person.Turns -= 1;
-                _people.Enqueue(person);
+
+                _people.Enqueue(new Person(person.Name, person.Turns - 1));
             }
 
             return person;
-        }
+        
     }
 
     public override string ToString()
--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -11,7 +11,8 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
     // run until the queue is empty
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: The actual result does not match the expected result. The TestTakingTurnsQueue is not managing the turns properly because is modifying the member variable _people, but it should instead create a new instance of the person class with the modified turns property. The class PersonQeue is adding to the zero index position instead of adding at the endeing of the queue when the Enqueue method is invoked.
+
     public void TestTakingTurnsQueue_FiniteRepetition()
     {
         var bob = new Person("Bob", 2);
@@ -43,7 +44,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
     // After running 5 times, add George with 3 turns.  Run until the queue is empty.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-    // Defect(s) Found: 
+    // Defect(s) Found: The actual result does not match the expected result. The TestTakingTurnsQueue is not managing the turns properly because is modifying the member variable _people, but it should instead create a new instance of the person class with the modified turns property. The class PersonQeue is adding to the zero index position instead of adding at the endeing of the queue when the Enqueue method is invoked.
     public void TestTakingTurnsQueue_AddPlayerMidway()
     {
         var bob = new Person("Bob", 2);
@@ -85,7 +86,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found:The actual result does not match the expected result. The zero turns case was not being handled by the method GetNextPerson of the TakingTurnsQueue class.
     public void TestTakingTurnsQueue_ForeverZero()
     {
         var timTurns = 0;
@@ -116,7 +117,8 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: The actual result does not match the expected result. The negative turns case was not being handled by the method GetNextPerson of the TakingTurnsQueue class.
+
     public void TestTakingTurnsQueue_ForeverNegative()
     {
         var timTurns = -3;
@@ -143,7 +145,7 @@ public class TakingTurnsQueueTests
     [TestMethod]
     // Scenario: Try to get the next person from an empty queue
     // Expected Result: Exception should be thrown with appropriate error message.
-    // Defect(s) Found: 
+    // Defect(s) Found: No errors found.
     public void TestTakingTurnsQueue_Empty()
     {
         var players = new TakingTurnsQueue();
