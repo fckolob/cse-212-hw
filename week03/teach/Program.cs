47e7b5cdbaacb0adb56d1c785794b69291b58ec9
fckolob <131497920+fckolob@users.noreply.github.com>
2025-07-18 17:17:46 -0300

    changes added

--- week03/teach/Program.cs
+++ week03/teach/Program.cs
@@ -4,12 +4,12 @@
 // Console.WriteLine("\n======================\nDisplay Sums\n======================");
 // DisplaySums.Run();
 
-// Console.WriteLine("\n======================\nBasketball\n======================");
-// Basketball.Run();
+Console.WriteLine("\n======================\nBasketball\n======================");
+Basketball.Run();
 
 // Uncomment and run as you get to the solution part
-Console.WriteLine("\n======================\nUnique Letters\n======================");
-UniqueLettersSolution.Run();
+// Console.WriteLine("\n======================\nUnique Letters\n======================");
+// UniqueLettersSolution.Run();
 
 // Console.WriteLine("\n======================\nDisplay Sums\n======================");
 // DisplaySumsSolution.Run();
